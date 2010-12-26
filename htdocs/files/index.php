9cf3e4197450b65f08b22596c95c99f8749ceab9
Weltraumschaf <ich@weltraumschaf.de>
2010-12-26 22:16:55 +0100

    Added music section.

---
+++ htdocs/files/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <ich@weltraumschaf.de> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ * @author    Weltraumschaf
+ * @copyright Copyright (c) 01.12.2010, Sven Strittmatter.
+ * @version   0.2
+ * @license   http://www.weltraumschaf.de/the-beer-ware-license.txt
+ */
+
+header('Location: ../');
--- htdocs/index.php
+++ htdocs/index.php
@@ -73,20 +73,35 @@
             </li>
         </ul>
 
-        <ul id="links">
-            <li>
-                <a href="./work.php" title="Arbeiten vom Weltraumschaf">
-                    Arbeiten</a> &ndash;
-                    <a href="./cite.php" title="Ein Zitat das Weltraumschaf gut findet">
-                    Zitat</a> &ndash;
-                <a href="./impressum.php" title="Lizenz und so&hellip;">Impressum</a> &ndash;
-                <a class="contact" title="E-Mail an mich">
-                    ich(at)weltraumschaf(dot)de</a> &ndash;
-                <a href="http://www.cacert.org/index.php?id=3" title="Braucht man f&uuml;r https://www.weltraumschaf.de">
-                    Root-Zertifikate von CAcert</a>
-            </li>
-        </ul>
+        <div id="links">
+            <a href="./work.php" title="Arbeiten vom Weltraumschaf">
+                Arbeiten</a> &ndash;
+                <a href="./cite.php" title="Ein Zitat das Weltraumschaf gut findet">
+                Zitat</a> &ndash;
+            <a href="#music" rel="musicLink" title="Musik vom Weltraumschaf">Musik</a> &ndash;
+            <a href="./impressum.php" title="Lizenz und so&hellip;">Impressum</a> &ndash;
+            <a id="contact" title="E-Mail an mich">
+                ich(at)weltraumschaf(dot)de</a> &ndash;
+            <a href="http://www.cacert.org/index.php?id=3" title="Braucht man f&uuml;r https://www.weltraumschaf.de">
+                Root-Zertifikate von CAcert</a>
+        </div>
 
+        <div id="music">
+            <h2>Musik vom Weltraumschaf</h2>
+            <p>Experimente mit <a href="http://www.ableton.com/" target="_blank">Ableton Live 8</a>,
+            <a href="http://www.korg.com/kaossilator" target="_blank">Korg Kaossilator</a>,
+            <a href="http://en.wikipedia.org/wiki/Korg_Polysix" target="_blank">Korg Polysix</a> und
+            <a href="http://www.use-audio.com/plugiator.htm" target="_blank">Plugiator</a>:
+                <ul>
+                    <li>Drums: <a href="files/Drums.mp3" target="_blank">mp3</a></li>
+                    <li>Leipzig: <a href="files/Leipzig.mp3" target="_blank">mp3</a></li>
+                    <li>Namaste: <a href="files/Namaste.mp3" target="_blank">mp3</a></li>
+                    <li>Unbenannt: <a href="files/Unbenannt.mp3" target="_blank">mp3</a> &ndash;
+                        <a href="files/Unbenannt.ogg" target="_blank">ogg</a></li>
+                </ul>
+            </p>
+        </div>
+    
         <?php include 'inc/body_javascript.html'; ?>
         
     </body>
