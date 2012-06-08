cdda82ced9b3f6f3a540af3d56d6f22b57e0b64a
Weltraumschaf <ich@weltraumschaf.de>
2012-06-08 13:06:56 +0200

    Improved amail version.

--- htdocs/js/jquery.amail.js
+++ htdocs/js/jquery.amail.js
@@ -2,45 +2,56 @@
  * jQuery aMail plug-in 1.0
  * (c) 2009 Max Ya  ( http://iHackWeb.com )
  * Inspired by epemail plugin/
+ * 
+ * http://ihackweb.com/archives/124
  *
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
- * 
- * HOW TO USE
- 
- THE DOM
-	example: $('#email').amail('||','|','NOSPAM');
- 
- THE PAGE CODE
-	Using Example:  <a class="amail" title="my email">lim||fromNOSPAMru|com</a>  
-	it will be <a title="my email" class="amail" href="mailto:lim@fromru.com">my email</a>
-	
  *
-*/
-$.fn.amail = function(sAt,sDot,sRepl){
+ * Example:
+ * $('#email').amail();
+ *
+ * Using Example: <a class="amail" title="my email">lim[at]fromNOSPAMru[dot]com</a>
+ * will become:   <a title="my email" class="amail" href="mailto:lim@fromru.com">my email</a>
+ *
+ * example: $('#email').amail('<at>','<dot>','FOOBAR');
+ *
+ * Using Example: <a class="amail" title="my email">lim<at>fromFOOBARru<dot>com</a>
+ * will become:   <a title="my email" class="amail" href="mailto:lim@fromru.com">my email</a>
+ */
+;
+(function($) {
+    $.fn.amail = function(sAt, sDot, sRepl) {
 
-	if (!sAt)
-		sAt = '||';
-		
-	if (!sDot)
-		sDot = '|';
-	
-	if (!sRepl)
-		sRepl = 'NOSPAM';
-	
-	
-	this.each(function() {
-					  
-		el = $(this);
-		var mail = el.text().replace(sAt,'@').replace(sDot,'.').replace(sRepl,'');
-		el.each(function(){
-		el.attr('href','mailto:' + mail);
-		if(el.attr('title')){
-			el.html(el.attr('title'));
-		}else{
-			el.html(mail);
-			}
-		});
-	});
-};
+        if (!sAt) {
+            sAt = '[at]';
+        }
+
+        if (!sDot) {
+            sDot = '[dot]';
+        }
+
+        if (!sRepl) {
+            sRepl = 'NOSPAM';
+        }
+
+        return this.each(function() {
+            var el   = $(this),
+                mail = el.text()
+                         .replace(sAt, '@')
+                         .replace(sDot, '.')
+                         .replace(sRepl, '');
+
+            el.each(function() {
+                el.attr('href', 'mailto:' + mail);
+
+                if (el.attr('title')) {
+                    el.html(el.attr('title'));
+                } else {
+                    el.html(mail);
+                }
+            });
+        });
+    };
+})(jQuery);
