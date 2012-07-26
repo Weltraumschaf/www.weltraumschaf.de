5313c3d1be424f01bde8301528a187ab13aaa666
Weltraumschaf <ich@weltraumschaf.de>
2012-07-26 09:28:16 +0200

    Fix impressum text.

--- htdocs/impressum.php
+++ htdocs/impressum.php
@@ -10,11 +10,11 @@
                dann k&ouml;nnen Sie das per E-Mail tun. Klicken Sie
                <a class="contact" title="hier">ich(at)weltraumschaf(dot)de</a>.</p>
 
-            <p>Wollen Sie mehr Informationen &uuml;ber dies Domain, dann k&ouml;nnen
+            <p>Wollen Sie mehr Informationen &uuml;ber diese Domain, dann k&ouml;nnen
                Sie diese bei der <a href="http://www.denic.de/" target="_blank">DeNIC</a> abrufen.</p>
 
             <p>Alles auf dieser Website wird unter der <a href="the-beer-ware-license.txt" target="_blank">
-               Beer-Ware license</a> ver&ouml;ffentlicht.</p>
+               Beer-Ware license</a> ver&ouml;ffentlicht, sofern nichts anderes angegeben ist.</p>
         </div>
         <script type="text/javascript">
         /* <![CDATA[ */
