67997fa6b4637fbab4d43f7a527e08f0c0d33e7e
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-09-04 19:48:05 +0200

    Enable content security poilicy.

--- htdocs/inc/head_include.php
+++ htdocs/inc/head_include.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self'"/>
 <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <meta name="robots" content="all"/>
 <link href="css/main.css" rel="stylesheet" type="text/css" media="screen"/>
